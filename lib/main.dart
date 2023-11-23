9eb36248881b0db102faf82d2e7750ccea1a5990
Amirhossein Rasouli <98424551+AM1RH3@users.noreply.github.com>
2023-11-23 14:34:37 +0330

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
